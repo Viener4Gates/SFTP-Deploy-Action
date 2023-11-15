c86f837b05a75044b3b74400f173db4fa5975628
Viener4Gates <149007425+Viener4Gates@users.noreply.github.com>
2023-11-15 11:52:21 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ if [ -z != ${10} ]; then
 
 	if test $9 == "true";then
   		echo 'Start delete remote files'
-		sshpass -p ${10} ssh -o StrictHostKeyChecking=no -p $3 $1@$2 del /s $6
+		sshpass -p ${10} ssh -o StrictHostKeyChecking=no -p $3 $1@$2 del /q /s $6
 	fi
 	if test $7 = "true"; then
   		echo "Connection via sftp protocol only, skip the command to create a directory"
